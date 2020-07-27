20cf6450ad8944920d5a1e5d486e14cf5802b50d
Light52566 <47724831+Light52566@users.noreply.github.com>
2020-07-27 16:13:39 +0300

    Update frame_cutter.py
    Added comments and replaced 'magic numbers' with variables for easier user calibration

--- frame_cutter.py
+++ frame_cutter.py
@@ -9,12 +9,15 @@ import os
 import pathlib
 
 
+frame_name = 'frame'    #can be edited to determine the general name for the frames that will be extracted/saved
+max_similarity = 0.85   #can be edited to choose the maximum similarity for the frames to have between each other
 
 # Construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--first", required=True, help="Directory of the video that will be used")
 ap.add_argument("-s", "--second", required=True, help="Directory for the frames to be saved")
 args = vars(ap.parse_args())
+
 
 # Open the Video file
 cap= cv2.VideoCapture(args["first"])
@@ -24,22 +27,22 @@ while(cap.isOpened()):
     if (ret == False):
         break
     if (i==0):
-        cv2.imwrite(os.path.join(args["second"],'frame'+str(i)+'.jpg'),frame)
+        cv2.imwrite(os.path.join(args["second"],frame_name + str(i)+'.jpg'),frame)  #initial frame saved
     else:
         similar = False
         imageB = frame
         grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-        for path in pathlib.Path(args["second"]).iterdir():
+        for path in pathlib.Path(args["second"]).iterdir(): #iteration to check similarity of current frame with previously saved frames
             if path.is_file():
                 imageA = cv2.imread(str(path))
                 grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
                 score = 1
-                (score, diff) = compare_ssim(grayA, grayB, full=True)
+                (score, diff) = compare_ssim(grayA, grayB, full=True)   #similarity determined with SSIM score
                 print(str(path)+"\t"+str(score))
-                if score > 0.7:
+                if score > max_similarity:
                     similar = True
         if not similar:
-            cv2.imwrite(os.path.join(args["second"],'frame'+str(i)+'.jpg'),frame)
+            cv2.imwrite(os.path.join(args["second"],frame_name + str(i)+'.jpg'),frame)
     i+=1
 cap.release()
 cv2.destroyAllWindows()
